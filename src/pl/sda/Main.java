4a814c04bdc102f4a11ad1f189dafce894fca60c
meystr <meystr@gmail.com>
2018-09-11 21:03:36 +0200

    You might know some pretty large perfect squares. But what about the NEXT one? Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer. If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.

---
+++ src/pl/sda/Main.java
@@ -0,0 +1,9 @@
+package pl.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+    NumberFun.findNextSquare(1290605625);
+
+    }
+}
---
+++ src/pl/sda/NumberFun.java
@@ -0,0 +1,8 @@
+package pl.sda;
+
+public class NumberFun {
+    public static long findNextSquare(long sq) {
+            long root = (long) Math.sqrt(sq);
+            return root * root == sq ? (root + 1) * (root + 1) : -1;
+    }
+}
